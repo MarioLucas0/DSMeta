83b03bdae62e5cc3da7edb6129a83b5b20c36539
Lucas <mariolucas.2000@outlook.com>
2022-09-07 18:47:41 -0300

    DataBase

---
+++ beckend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -0,0 +1,85 @@
+package com.devsuperior.dsmeta.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long id;
+     private String sellerName;
+     private Integer visited;
+     private Integer deals;
+     private Double amount;
+     private LocalDate date;
+
+
+    public Sale(Long id, String sellerName, Integer visited, Integer deals, Double amount, LocalDate date) {
+        this.id = id;
+        this.sellerName = sellerName;
+        this.visited = visited;
+        this.deals = deals;
+        this.amount = amount;
+        this.date = date;
+    }
+
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSellerName() {
+        return this.sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public Integer getVisited() {
+        return this.visited;
+    }
+
+    public void setVisited(Integer visited) {
+        this.visited = visited;
+    }
+
+    public Integer getDeals() {
+        return this.deals;
+    }
+
+    public void setDeals(Integer deals) {
+        this.deals = deals;
+    }
+
+    public Double getAmount() {
+        return this.amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return this.date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+
+}
